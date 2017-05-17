76070d9291558b5901e5e0fbd5c88dfb4afd49fd
pote <poteland@gmail.com>
2017-05-17 12:10:39 -0300

    Updated to latest client

--- main.go
+++ main.go
@@ -13,14 +13,23 @@ func main() {
   secret := flag.String("s", "", "Philote server's JWT secret used for authentication.")
   server := flag.String("p", "ws://localhost:6380", "Philote server")
   channel := flag.String("c", "test", "channel you want to connect to")
+  token := flag.String("t", "", "Auth token, if you want to use a specific one")
 
   flag.Parse()
 
-  client, err := philote.NewClient(*server, *secret, []string{*channel}, []string{*channel}); if err != nil {
+  auth := *token; if auth == "" {
+    var err error
+    auth, err = philote.NewToken(*secret, []string{*channel}, []string{*channel}); if err != nil {
+      log.Fatal(err)
+    }
+  }
+
+  client, err := philote.NewClient(*server, auth); if err != nil {
     log.Fatal(err)
   }
 
   log.Printf("Successful connection to %v on the #%v channel\n", *server, *channel)
+  log.Printf("Token: %v\n", client.Token)
   messages := client.NewPhilote()
 
   go func() {
