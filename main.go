47fbeeb7610602139f65ff6f4230149c30120e37
pote <poteland@gmail.com>
2017-08-20 21:31:15 -0300

    Adds info query

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import(
   "bufio"
   "flag"
+  "fmt"
+  "io/ioutil"
   "log"
+  "net/http"
   "os"
 
   "github.com/pote/philote-go"
+  "github.com/dgrijalva/jwt-go"
 )
 
 func main() {
   secret := flag.String("s", "", "Philote server's JWT secret used for authentication.")
-  server := flag.String("p", "ws://localhost:6380", "Philote server")
+  server := flag.String("p", "localhost:6380", "Philote server")
   channel := flag.String("c", "test", "channel you want to connect to")
   token := flag.String("t", "", "Auth token, if you want to use a specific one")
+  info := flag.Bool("info", false, "If passed, queries Philote for running info and exits")
 
   flag.Parse()
 
@@ -24,7 +29,30 @@ func main() {
     }
   }
 
-  client, err := philote.NewClient(*server, auth); if err != nil {
+  if *info {
+    request, err := http.NewRequest("GET", "http://" + *server + "/api/info", nil); if err != nil {
+      log.Fatal(err)
+    }
+
+    auth := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"api": true})
+    token, err := auth.SignedString([]byte(*secret)); if err != nil {
+    log.Fatal(err)
+    }
+
+    request.Header.Add("Authorization", "Bearer " + token)
+    res, err := http.DefaultClient.Do(request); if err != nil {
+      log.Fatal(err)
+    }
+
+    body, err := ioutil.ReadAll(res.Body); if err != nil {
+      log.Fatal(err)
+    }
+
+    fmt.Print(string(body))
+    os.Exit(0)
+  }
+
+  client, err := philote.NewClient("ws://" + *server, auth); if err != nil {
     log.Fatal(err)
   }
 
